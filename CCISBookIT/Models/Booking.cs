19f5b674d94db146c07270a585413e68d6ca2815
RenzCodeRepo <renzbaladjay25@gmail.com>
2024-06-26 21:58:45 +0800

    Added Booking, Rooms, User Models

---
+++ CCISBookIT/Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CCISBookIT.Models
+{
+    public class Booking
+    {
+        [Key]
+        public string bookingId { get; set; }
+        [ForeignKey("User")]
+        public string facultyId { get; set; }
+        public DateTime date { get; set; }
+        public TimeOnly startTime { get; set; }
+        public int duration { get; set; }
+        public TimeOnly endTime { get; set; }
+        [ForeignKey("Rooms")]
+        public string roomNo { get; set; }
+        public string purpose { get; set; }
+        public string status { get; set; }
+
+    }
+}
---
+++ CCISBookIT/Models/rooms.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CCISBookIT.Models
+{
+    public class rooms
+    {
+        [Key]
+        public string roomNo { get; set; }
+        public string roomType { get; set; }
+
+    }
+}
