46a2872f5e667f6de176af301f79e340c7a1672e
RenzCodeRepo <renzbaladjay25@gmail.com>
2024-06-29 21:20:29 +0800

    Deleted the existing migration folder, recreated the initial migration, "Add-Migration InitialCreate", then updated the database "Update-Database".

--- CCISBookIT/Models/Booking.cs
+++ CCISBookIT/Models/Booking.cs
@@ -10,21 +10,19 @@ namespace CCISBookIT.Models
         //structure of the table
         [Key]
         public string BookingId { get; set; }
-   
-        public User User { get; set; } // Navigation property
         public DateTime Date { get; set; }
         public TimeOnly StartTime { get; set; }
-        public int Duration { get; set; }
+        public Double Duration { get; set; }
         public TimeOnly EndTime { get; set; }
         public string Purpose { get; set; }
         public Status Status { get; set; }
 
         [ForeignKey("Room")]
-        public int RoomNo { get; set; }
+        public string RoomNo { get; set; }
         public Room Room { get; set; } // Navigation property
 
         [ForeignKey("User")]
         public string FacultyId { get; set; }
-        
+        public User User { get; set; } // Navigation property
     }
 }
