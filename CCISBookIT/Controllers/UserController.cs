d40f4cee5579ce5ac119a4814c2330c9d8af1e99
Zabdiel <zabdielmanzana@gmail.com>
2024-07-03 05:15:17 +0800

    added forgot,signup,login forms and css and configurations in Usercontroller. the navigation bar and about us footer is not remove.

--- CCISBookIT/Controllers/UserController.cs
+++ CCISBookIT/Controllers/UserController.cs
@@ -1,6 +1,9 @@
 ﻿using CCISBookIT.Data;
+using CCISBookIT.Data.Enum;
+using CCISBookIT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace CCISBookIT.Controllers
 {
@@ -17,5 +20,35 @@ namespace CCISBookIT.Controllers
             var user = _context.Users.OrderBy(u => u.FacultyID).ToList(); 
             return View(user);
         }
+
+        //Simple Get Request: User/Create
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+       /* [HttpPost]
+        public async Task<IActionResult> Signup([Bind("FacultyID,FullName,Email,PhoneNumber")] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+        }
+       */
+
+        public IActionResult Login()
+        {
+            return View(); 
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View(); 
+        }
+
+        
+        
     }
-}
+ }
